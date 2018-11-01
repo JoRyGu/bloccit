0f32b2644bceb4af3ce291119e322cf11d0bb199
Josh Gude <jgude@Joshs-MacBook-Pro.local>
2018-11-01 13:21:37 -0400

    added crud functionality to db

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,13 +1,60 @@
 const Topic = require("./models").Topic;
 
 module.exports = {
-  getAllTopics(callback){
-    return Topic.all()
-    .then((topics) => {
+  async getAllTopics(callback) {
+    try {
+      let topics = await Topic.all();
       callback(null, topics);
-    })
-    .catch((err) => {
+    } catch (err) {
       callback(err);
-    })
+    }
+  },
+
+  async addTopic(newTopic, callback) {
+    try {
+      let topic = await Topic.create({
+        title: newTopic.title,
+        description: newTopic.description
+      });
+      callback(null, topic);
+    } catch (err) {
+      callback(err);
+    }
+  },
+
+  async getTopic(id, callback) {
+    try {
+      let topic = await Topic.findById(id);
+      callback(null, topic);
+    } catch (err) {
+      callback(err);
+    }
+  },
+
+  async deleteTopic(id, callback) {
+    try {
+      let topic = await Topic.destroy({ where: { id } });
+      callback(null, topic);
+    } catch (err) {
+      callback(err);
+    }
+  },
+
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findById(id).then(topic => {
+      if (!topic) {
+        return callback("Topic not found");
+      }
+      topic
+        .update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch(err => {
+          callback(err);
+        });
+    });
   }
-}
+};
