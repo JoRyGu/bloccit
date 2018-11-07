d48d41e876b9f029e08d9266b765f480ba544895
Josh Gude <jgude@Joshs-MacBook-Pro.local>
2018-11-07 00:54:10 -0500

    added validation to post resource

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -71,6 +71,25 @@ describe("routes : posts", () => {
           });
       });
     });
+
+    it("should not create a new post that fails validations", done => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+      request.post(options, (err, res, body) => {
+        Post.findOne({where: {title: "a"}}).then(post => {
+          expect(post).toBeNull();
+          done();
+        }).catch(err => {
+          console.log(err);
+          done();
+        })
+      })
+    })
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -137,12 +156,14 @@ describe("routes : posts", () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "This is a new body"
         }
       };
       request.post(options, (err, res, body) => {
         expect(err).toBeNull();
         Post.findOne({ where: { id: this.post.id } }).then(post => {
+          console.log(post.title, post.body);
           expect(post.title).toBe("Snowman Building Competition");
           done();
         });
